323ad44637cfea1eaf5edc42f00950c36863f1d6
TetsuoMiyake <tmky1919mnstwebcamp@gmail.com>
2021-10-08 14:25:13 +0000

    suggest-items-start

--- app/controllers/daily_reports_controller.rb
+++ app/controllers/daily_reports_controller.rb
@@ -5,7 +5,7 @@ class DailyReportsController < ApplicationController
   end
 
   def index
-    @daily_reports = DailyReport.all
+    @daily_reports = DailyReport.all.order(visit_date: :desc)
   end
 
   def show
@@ -13,17 +13,19 @@ class DailyReportsController < ApplicationController
   end
 
   def edit
-
+    @daily_report = DailyReport.find(params[:id])
   end
 
   def create
     daily_report = DailyReport.new(daily_report_params)
     daily_report.save
-    redirect_to daily_reports_path
+    redirect_to daily_report_path(daily_report.id)
   end
 
   def update
-
+    daily_report = DailyReport.find(params[:id])
+    daily_report.update(daily_report_params)
+    redirect_to daily_report_path(daily_report.id)
   end
 
   def destroy
@@ -32,6 +34,6 @@ class DailyReportsController < ApplicationController
 
   private
   def daily_report_params
-    params.require(:daily_report).permit(:user_id, :customer_id, :content)
+    params.require(:daily_report).permit(:user_id, :customer_id, :content, :visit_date)
   end
 end
