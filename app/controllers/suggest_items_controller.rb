f6c02f73e4f1decde0e6cbcd48843ea2dc1860b7
TetsuoMiyake <tmky1919mnstwebcamp@gmail.com>
2021-10-22 14:56:13 +0000

    layout-userpage-customer

--- app/controllers/suggest_items_controller.rb
+++ app/controllers/suggest_items_controller.rb
@@ -3,13 +3,13 @@ class SuggestItemsController < ApplicationController
   def create
     suggest_item = SuggestItem.new(suggest_item_params)
     suggest_item.save
-    redirect_to customers_path
+    redirect_to customer_path(suggest_item.customer_id)
   end
 
   def update
     suggest_item = SuggestItem.find(params[:id])
     suggest_item.update(suggest_item_params)
-    redirect_to customers_path
+    redirect_to customer_path(suggest_item.customer_id)
   end
 
   private
